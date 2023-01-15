91971850e606ec5322f211edb5fe7405b81794a4
Jessica Fairbairn <jfairbairn1@sheffield.ac.uk>
2023-01-15 23:09:49 +0000

    Experiment around using web extensions

--- page/script.js
+++ page/script.js
@@ -21,14 +21,14 @@ async function loadDuplicateTabList(fullTabList) {
     for (const [key, count] of Object.entries(siteSet.getResultsWithMin(2))) {
 
         let tabs = await getTabsWithUrlAsync(key);
-        debugger;
+        
 
         let url_li = document.createElement("li");
         url_li.innerText = `${key}: ${count}`;
 
         let inner_list = document.createElement("ul");
         for (let tab of tabs) {
-            let li = createListItemForTab(tab);
+            let li = new TabItem(tab);
             inner_list.appendChild(li);
         }
         url_li.appendChild(inner_list);
@@ -72,7 +72,7 @@ function createListItemForTab(tab) {
         timeString = "just now"
     }
 
-    let li = document.createElement("li");
+    let li = document.createElement("tab-item");
     li.className = "tab-item"
     li.id = `tab-${tab.id}`
     li.innerText = `Last accessed ${timeString} ${tab.pinned ? '📌' : ''}`;
@@ -133,3 +133,44 @@ async function closeAllTabsInDomain(domain) {
     return await loadDuplicateTabList(fullTabList);
         
 }
+
+class TabItem extends HTMLLIElement {
+    
+
+    constructor(tab) {
+        super();
+        let template = document.getElementById("tab-item");
+        let templateContent = template.content;
+        // const shadowRoot = this.attachShadow({ mode: "open" });
+        // shadowRoot.appendChild(templateContent.cloneNode(true));
+        if (!tab) {
+            return 
+        }
+        const timeDifference = Date.now() - tab.lastAccessed;
+        let secondsDiff = timeDifference/60000
+        let minutesDiff = secondsDiff/60;
+        let hoursDiff = minutesDiff/60
+        let daysDiff = hoursDiff/24;
+
+        let timeString;
+        if (daysDiff >= 1){
+            timeString = `${Math.floor(daysDiff)} days ago`
+        } else if (hoursDiff >= 1) {
+            timeString = `${Math.floor(hoursDiff)} hours ago`
+        } else if (minutesDiff >= 1) {
+            timeString = `${Math.floor(minutesDiff)} minutes ago`
+        } else {
+            timeString = "just now"
+        }
+
+        
+        this.className = "tab-item"
+        this.id = `tab-${tab.id}`
+        this.innerText = `Last accessed ${timeString} ${tab.pinned ? '📌' : ''}`;
+        let button = document.createElement("button");
+        button.innerText = '✖';
+        button.onclick = () => closeTabById(tab.id);
+        this.appendChild(button);
+    }
+}
+customElements.define('tab-item', TabItem, {extends: 'li'});
