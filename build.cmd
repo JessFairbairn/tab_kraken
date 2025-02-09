59c131b83306dbbb5e5c1a84cb218b19cb1707df
Jess Fairbairn <jessfairbairn91@gmail.com>
2025-02-09 18:46:17 +0000

    Updated scripts

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+call npm install
 mkdir external
-copy node_modules/webextension-polyfill/dist/browser-polyfill.min.js external/browser-polyfill.min.js
+copy node_modules\webextension-polyfill\dist\browser-polyfill.min.js external\browser-polyfill.min.js
 web-ext build --overwrite-dest
