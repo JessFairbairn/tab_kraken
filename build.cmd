fce609959f4abe9470dac7d8d461f6f68a625377
Jess Fairbairn <jessfairbairn91@gmail.com>
2025-02-16 23:15:32 +0000

    Attempt to make build process less messy

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-call npm install
+call npm install --save=false
 mkdir external
 copy node_modules\webextension-polyfill\dist\browser-polyfill.min.js external\browser-polyfill.min.js
 web-ext build --overwrite-dest
